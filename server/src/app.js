c008e97e3ad88f3b79807f5445f927f0601ade8a
Aniket Singh <aniketsingh7141340@gmail.com>
2026-05-02 18:34:22 +0530

    add versioning to api routes

--- server/src/app.js
+++ server/src/app.js
@@ -11,9 +11,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/auth", authRoutes);
-app.use("/api/projects", projectRoutes);
-app.use("/api/tasks", taskRoutes);
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/projects", projectRoutes);
+app.use("/api/v1/tasks", taskRoutes);
 
 app.get("/", (req, res) => {
   res.send("Team Task Manager API is running...");
